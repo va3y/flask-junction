3410fb8cc09b00749db0da5a2bb1c3e164e124c5
Valery Ivanov <valeryivanov@Valerys-MacBook-Pro.local>
2023-11-11 16:12:56 +0200

    add cors

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify
 import os
+from flask_cors import CORS, cross_origin
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@cross_origin()
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"Choo Choo ": "Welcome to cross origin Flask app 🚅"})
 
 
 if __name__ == '__main__':
